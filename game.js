22585add887e6839d81ccb355c148cde4ee27397
RavenHex1296 <suanoo5196@gmail.com>
2022-02-28 23:20:20 +0000

    bare minimum info

--- game.js
+++ game.js
@@ -1,8 +1,24 @@
+
+class Ship {
+    constructor(coords) {
+        this.coords = coords;
+    }
+};
+
 class Game {
     constructor(clientSockets) {
         this.clientSockets = clientSockets;
 
-        this.state = null;
+        this.board = [];
+
+        for(let i = 0; i < 7; i++){
+            this.board.push([])
+            
+            for(let j = 0; j < 7; j++){
+                this.board[i].push([])
+            }
+        }
+        this.ships = []
     }
 
     start() {
@@ -19,30 +35,56 @@ class Game {
         }, 200);  
     }
 
-    generateRandomGameState() {
-        let board = {
-            numRows: 20,
-            numCols: 20,
-            spaces: []
-        };
+    // generateRandomGameState() {
+    //     let board = {
+    //         numRows: 20,
+    //         numCols: 20,
+    //         spaces: []
+    //     };
 
-        board.spaces = new Array(board.numRows);
-        for(let i = 0; i < board.numRows; i++) {
-            board.spaces[i] = new Array(board.numCols);
-        }
+    //     board.spaces = new Array(board.numRows);
+    //     for(let i = 0; i < board.numRows; i++) {
+    //         board.spaces[i] = new Array(board.numCols);
+    //     }
+
+    //     for(let i = 0; i < board.numRows; i++) {
+    //         for(let j = 0; j < board.numCols; j++) {        
+    //             let r = this.getRandomInteger(1, 20);
+    //             board.spaces[i][j] = r;
+    //         }
+    //     }    
+
+    //     let gameState = {
+    //         board
+    //     };
+
+    //     return gameState;
+    // }
+
+    initializeGame() {
+        //make board
+        //give ships to players
+        let board = []
 
         for(let i = 0; i < board.numRows; i++) {
-            for(let j = 0; j < board.numCols; j++) {        
-                let r = this.getRandomInteger(1, 20);
-                board.spaces[i][j] = r;
+            for(let j = 0; j < board.numCols; j++) {
+                board[j][i] = [];
             }
-        }    
+        }
+        
+        this.ships.push(new Ship([3,0]))
+    }
 
-        let gameState = {
-            board
-        };
+    movementPhase() {
+        this.ships[0].coords[1] += 1 
+    }
 
-        return gameState;
+    run(numTurns) {
+        this.initializeGame()
+        for(let i = 0; i<numTurns; i++){
+            this.movementPhase()
+            this.turn ++;
+        }
     }
 
     getRandomInteger(min, max) {
@@ -51,7 +93,7 @@ class Game {
 
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
-}
+};
 
 module.exports = Game;
 
