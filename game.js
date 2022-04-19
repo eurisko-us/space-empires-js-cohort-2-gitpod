6d8272b2dedc4f20bf9fbed8a800ff0a29338f2e
Charlie Weinberger <364charliebrown@gmail.com>
2022-04-19 23:30:29 +0000

    fixed bug

--- game.js
+++ game.js
@@ -116,8 +116,6 @@ class Game {
                 
                 ship.coords[0] += option[0];
                 ship.coords[1] += option[1];
-                this.log.shipMovement(oldCoords, ship.coords, ship.playerNum, ship.name, ship.shipNum)
-
                 this.log.shipMovement(oldCoords, ship.coords, ship.playerNum, ship.name, ship.shipNum);
 
                 ship.updateCoords(ship.coords);
