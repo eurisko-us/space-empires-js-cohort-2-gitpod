9dd018ec07eb1e37b75c9783ba0caa7832e276d3
RavenHex1296 <suanoo5196@gmail.com>
2022-05-17 21:59:04 +0000

    made test folder

--- player.js
+++ player.js
@@ -20,38 +20,4 @@ class Player {
     }
 };
 
-/*
-    getDistance(point1, point2) {
-        let dx = Math.pow((point1[0] - point2[0]), 2)
-        let dy = Math.pow((point1[1] - point2[1]), 2)
-    
-        return Math.sqrt(dx + dy)
-    
-    }
-
-    getDesiredLocation(desiredLocation, ship, possibleTranslations){
-        let minDistance = 100000;
-        let minDistanceTranslation = possibleTranslations[0];
-
-        for (let translation of possibleTranslations) {
-            let shipCoords = [...ship.coords];
-
-            let newCoords = [shipCoords[0] + translation[0], shipCoords[1] + translation[1]]
-            let newDistance = this.getDistance(newCoords, desiredLocation)
-
-            if (newDistance < minDistance){
-                minDistance = newDistance
-                minDistanceTranslation = translation
-            }
-
-        }
-
-        return minDistanceTranslation
-    }
-
-    chooseTranslation(ship, translations) {
-        return this.getDesiredLocation([3, 3], ship, translations)
-    }
-*/
-
 module.exports = Player;
