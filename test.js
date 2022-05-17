9da034bb02496181819da2f5e6c9cc087a34b6df
JHpusd <justinhongdebate@gmail.com>
2022-05-17 22:02:09 +0000

    test file

--- test.js
+++ test.js
@@ -1,4 +1,17 @@
-
+const Game = require('./game');
+const Player = require('./player');
+const TestPlayer = require('./testPlayer')
+const ships = require('./ships');
+const Scout = ships.Scout;
+const BattleCruiser = ships.BattleCruiser;
+const Battleship = ships.Battleship;
+const Cruiser = ships.Cruiser;
+const Destroyer = ships.Destroyer;
+const Dreadnaught = ships.Dreadnaught;
+const boardSize = 7;
+const players = [new Player(1), new Player(2)];
+const maxTurns = 10;
+var assert = require('assert');
 
 // test friendly fire
 // test that ships cant move off board
