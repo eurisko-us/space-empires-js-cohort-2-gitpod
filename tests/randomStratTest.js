0ef25c36b0fead968cf0866276a6f4f8535316af
Charlie Weinberger <364charliebrown@gmail.com>
2022-09-09 20:36:30 +0000

    changed readme

--- tests/randomStratTest.js
+++ tests/randomStratTest.js
@@ -1,7 +1,6 @@
 import Game from '../src/game.js';
 import Strategy from '../strategies/strategy.js';
 
-
 for (let i = 0; i < 1000; i++) {
 
     const game = new Game(null, [new Strategy(), new Strategy()], {'Scout': 1}, 1);
