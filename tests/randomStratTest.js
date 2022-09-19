fea8c6a3dd4e254b611427f0384b1616ad7dd9bf
walliuswilliam <william.wallius@gmail.com>
2022-09-19 21:03:41 +0000

    updated random tests

--- tests/randomStratTest.js
+++ tests/randomStratTest.js
@@ -1,9 +1,13 @@
 import Game from '../src/game.js';
 import Strategy from '../strategies/strategy.js';
+import RandomStrategy from '../strategies/randomStrat.js';
+
+// let strats = [new Strategy(), new Strategy()]
+let strats = [new RandomStrategy(), new RandomStrategy()]
 
 for (let i = 0; i < 1000; i++) {
 
-    const game = new Game(null, [new Strategy(), new Strategy()], {'Scout': 1}, 1);
+    const game = new Game(null, strats, {'Scout': 1}, 1);
     game.initializeGame();
     game.start();
 
