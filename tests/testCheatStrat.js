f7de9f4424f50b3dbb81b48d2534201c53321edd
RavenHex1296 <suanoo5196@gmail.com>
2022-10-25 20:28:07 +0000

    fixed test for cheat strat

--- tests/testCheatStrat.js
+++ tests/testCheatStrat.js
@@ -1,8 +1,10 @@
 import Game from '../src/game.js';
 import RushStrat from '../strategies/rushStrat.js';
+import assert, { deepEqual } from 'assert';
 
-const strats1 = [new RushStrat(), new BasicStrat()];
-const game = new Game(null, strats1, {'Scout': 1});
+const strats1 = [new RushStrat(), new RushStrat()];
+const game = new Game(null, strats1);
 game.initializeGame();
 game.start();
 assert (game.winner != 2, "Cheater Strategy won when it should have lost")
+console.log("PASSED")
