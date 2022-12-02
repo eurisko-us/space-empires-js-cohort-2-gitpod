2b3f7e3ab20d17c2587e3b71f030632cfce05510
RavenHex1296 <suanoo5196@gmail.com>
2022-12-02 20:53:49 +0000

    made competition strats folder

---
+++ competitionStrategies/cayden.js
@@ -0,0 +1,28 @@
+import ParentStrat from './parentStrat.js';
+
+class CaydenStrat extends ParentStrat {
+    
+    constructor() {
+        super(ParentStrat);
+        this.name = 'rush';
+    }
+
+    chooseTranslation(ship, translations) {
+        let targetCoords = this.getOpponentHomeColonyCoords(ship);
+        return this.minDistanceTranslation(ship, translations, targetCoords);
+    }
+
+    chooseTarget(shipInfo, combatOrder) {
+        let opponentShips = combatOrder.filter(ship => ship.playerNum != shipInfo.playerNum && ship.hp > 0);
+        return opponentShips[0];
+    }
+
+    buyShips(cpBudget) {
+        if (this.turn == 0) return [{"Dreadnaught": 5}];
+        if (cpBudget > 50) return [{"Dreadnaught": 1}];
+        return [];
+    } 
+
+}
+
+export default CaydenStrat;
