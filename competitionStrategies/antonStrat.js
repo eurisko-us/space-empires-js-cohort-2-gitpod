2307d33bef21318a4d8cfe8aca390980cc2954e7
anton-perez <anton.sungho.perez@gmail.com>
2022-12-02 21:34:28 +0000

    added anton strat

---
+++ competitionStrategies/antonStrat.js
@@ -0,0 +1,31 @@
+import ParentStrat from './parentStrat.js';
+
+class AntonStrat extends ParentStrat {
+    
+    constructor() {
+        super(ParentStrat);
+        this.name = 'turtle';
+    }
+
+    chooseTranslation(ship, translations) {
+        if (this.turn == 1 || ship.shipNum > 5) {        
+            let targetCoords = this.getOpponentHomeColonyCoords(ship);
+            return this.minDistanceTranslation(ship, translations, targetCoords);
+        }
+        return [0, 0];
+    }
+
+    chooseTarget(shipInfo, combatOrder) {
+        let opponentShips = combatOrder.filter(ship => ship.playerNum != shipInfo.playerNum && ship.hp > 0);
+        return this.random(opponentShips);
+    }
+
+    buyShips(cpBudget) {
+        if (this.turn == 0) return [{"Dreadnaught": 5}];
+        if (cpBudget > 30) return [{"Dreadnaught": 1}];
+        return [];
+    }
+
+}
+
+export default AntonStrat;
