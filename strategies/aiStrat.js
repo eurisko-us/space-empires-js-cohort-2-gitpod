69c1ae967ad209a883a5d494b3e3c180d96c56fc
RavenHex1296 <suanoo5196@gmail.com>
2023-02-16 21:35:17 +0000

    merged williams movement ai stuff with mine, checking for obvious erros

--- strategies/aiStrat.js
+++ strategies/aiStrat.js
@@ -7,7 +7,13 @@ class AiStrat extends ParentStrat {
         this.name = 'AI';
         this.movementWeights = {} // set random number 0-1 Math.random
 
-        for (let factor of []){ //input list of movement factors here
+        let movementFactors = ["hp", "atk", "df", "shipClass", "coords", 
+                                "playerNum", "shipNum", "cpCost", "maintCost", 
+                                "distHC", "distOppHC", "changeDistHC", "changeDistOppHC", 
+                                "changeDistNearOwnShip", "changeDistNearOppShip", 
+                                "changeDistNearFreePlanet"]
+
+        for (let factor of movementFactors){ //input list of movement factors here
             this.movementWeights[factor] = Math.random()
         }
     }
@@ -61,12 +67,76 @@ class AiStrat extends ParentStrat {
 
     }
 
+    changeInDistance(start, end, coords) { 
+        return Math.abs(this.dist(start, coords) - this.dist(end, coords));
+    }
+
+    translate(x, y) {
+        return x.map((_, i) => x[i] + y[i]);
+    }
+
+    getMovementFactors(ship, translation) {
+        let shipFactorDict = {};
+        let shipClasses = ['A', 'B', 'C', 'D', 'E', 'Z'];
+        
+        let newCoords = this.translate(ship.coords, translation);
+        let shipCoords = this.getAllShips(null, ship.playerNum);
+        let oppShipCoords = this.getAllShips(null, 3-ship.playerNum);
+        let hcCoords = this.getColonyCoords(ship, true, false);
+        let oppHCCoords = this.getColonyCoords(ship, true, true);
+        let freePlanetsCoords = this.getFreePlanetsCoords();
+
+        for (var prop in ship) {
+            if (Object.prototype.hasOwnProperty.call(ship, prop)) {
+                if (!['name', 'id', 'objType'].includes(prop)) {
+                    if (prop == 'shipClass') {
+                        shipFactorDict[prop] = shipClasses.indexOf(ship[prop])
+                    }
+                    else {
+                        shipFactorDict[prop] = ship[prop];
+                    }
+                }
+            }
+        }
+
+        shipFactorDict['distHC'] = this.dist(ship.coords, hcCoords);
+        shipFactorDict['distOppHC'] = this.dist(ship.coords, oppHCCoords);
+
+        shipFactorDict['changeDistHC'] = this.changeInDistance(newCoords, ship.coords, hcCoords);
+        shipFactorDict['changeDistOppHC'] = this.changeInDistance(newCoords, ship.coords, oppHCCoords);
+
+        if (shipCoords.length === 0) {
+            shipFactorDict['changeDistNearOwnShip'] = shipFactorDict['changeDistHC'];
+        }
+        else {
+            let nearestShipCoords = this.getNearestCoords(ship, shipCoords);
+            shipFactorDict['changeDistNearOwnShip'] = this.changeInDistance(ship.coords, newCoords, nearestShipCoords);
+        }
+
+        if (oppShipCoords.length === 0) {
+            shipFactorDict['changeDistNearOppShip'] = shipFactorDict['changeDistOppHC'];
+        }
+        else {
+            let nearestOppShipCoords = this.getNearestCoords(ship, oppShipCoords);
+            shipFactorDict['changeDistNearOppShip'] = this.changeInDistance(ship.coords, newCoords, nearestOppShipCoords);
+        }
+
+        if (freePlanetsCoords.length === 0) {
+            shipFactorDict['changeDistNearFreePlanet'] = 0;
+        }
+        else {
+            let nearestFreePlanetCoords = this.getNearestCoords(ship, freePlanetsCoords);
+            shipFactorDict['changeDistNearFreePlanet'] = this.changeInDistance(ship.coords, newCoords, nearestFreePlanetCoords);
+        }
+        
+        return shipFactorDict;
+    }
+
     chooseTranslation(ship, translations) {
         let movementScores = this.getAllScores(shipInfo, translations, "movement")
         let maxScoreMove = this.getMaxScoreMove(movementScores)
         return maxScoreMove
     }
-
 
     getCombatFactors(shipInfo, opponentShipInfo) {
         let factors = {};
@@ -104,8 +174,7 @@ class AiStrat extends ParentStrat {
 
     buyShips(cpBudget) {
 
-}
-
+    }
 
 }
 
