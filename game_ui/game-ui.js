20c67e46e35bb4d293efdb2c1e805aaf56503f87
Charlie Weinberger <364charliebrown@gmail.com>
2023-04-13 16:20:26 +0000

    differentiated board-stagger-3 from -2

--- game_ui/game-ui.js
+++ game_ui/game-ui.js
@@ -134,30 +134,19 @@ function createEventListeners() {
 
 }
 
-function isBlank(i, j) {
-    let modifiedIndex = j - i%2;
+function isBlank(x, y) {
+    let modifiedIndex = y - x%2;
     return modifiedIndex == -1 || modifiedIndex == 7;
 }
 
-// function boardToUICoords(x, y) {
-//     return [x, y + x%2];
-// }
-
-// function UIToBoardCoords(i, j) {
-//     return [i, j - i%2];
-// }
-
 function updateObjType(objType, colors, innerHTML) {
 
-    for (let i = 0; i < 7; i++) {
-        for (let j = 0; j < 7; j++) {
-            for (let obj of game.board[j][i]) {
+    for (let x = 0; x < 7; x++) {
+        for (let y = 0; y < 7; y++) {
+            for (let obj of game.board[y][x]) {
                 if (obj.objType === objType) {
 
-                    // let x, y = boardToUICoords(i, j);
-                    let x = i; // 
-                    let y = j; //
-                    let shipNum = game.board[i][j][0].playerNum;
+                    let shipNum = game.board[x][y][0].playerNum;
                     let cell = boardHTML.rows[x].cells[y];
 
                     cell.style.backgroundColor = colors[shipNum - 1];
@@ -171,11 +160,8 @@ function updateObjType(objType, colors, innerHTML) {
 }
 
 function resetBoard() {
-    for (let i = 0; i < 7; i++) {
-        for (let j = 0; j < 7; j++) {
-            // let x, y = boardToUICoords(i, j);
-            let x = i; // 
-            let y = j; //
+    for (let x = 0; x < 7; x++) {
+        for (let y = 0; y < 7; y++) {
             let cell = boardHTML.rows[x].cells[y];
             cell.style.backgroundColor = 'gray';
             cell.innerHTML = '';
@@ -192,13 +178,10 @@ function updateLogs() {
     }
 }
 
-function updateSquareInfo(i, j) {
-
-    // let x, y = UIToBoardCoords(i, j);
-    let x = i; // 
-    let y = j; //
+function updateSquareInfo(x, y) {
 
     squareInfoHTML.innerHTML = `Objects on coordinate (${x}, ${y}):<br><br>`;
+    
     for (let obj of game.board[y][x]) {
 
         squareInfoHTML.innerHTML += `<strong>${obj.id}</strong>: hp: ${obj.hp}`;
@@ -218,60 +201,68 @@ function updateSquareInfo(i, j) {
 <html>
 <head>
 
+
 <style>
 
+
 .main {
-  display: flex;
-  --size: 100px;
-  --margin: 4px;
-  --gradient: calc(var(--size) * 1.732 + 4 * var(--margin) - 1px); 
+ display: flex;
+ --size: 100px;
+ --margin: 4px;
+ --gradient: calc(var(--size) * 1.732 + 4 * var(--margin) - 1px);
 }
+
 
 .container {
-  font-size: 0;
+ font-size: 0;
 }
+
 
 .container div {
-  width: var(--size);
-  margin: var(--margin);
-  height: calc(var(--size) * 1.1547);
-  display: inline-block;
-  clip-path: polygon(0% 25%, 0% 75%, 50% 100%, 100% 75%, 100% 25%, 50% 0%);
-  margin-bottom: calc(var(--margin) - var(--size) * 0.2886);
+ width: var(--size);
+ margin: var(--margin);
+ height: calc(var(--size) * 1.1547);
+ display: inline-block;
+ clip-path: polygon(0% 25%, 0% 75%, 50% 100%, 100% 75%, 100% 25%, 50% 0%);
+ margin-bottom: calc(var(--margin) - var(--size) * 0.2886);
 }
+
 
 .container::before {
-  content: "";
-  width: calc(var(--size)/2 + var(--margin));
-  float: left;
-  height: 100%;
-  shape-outside: repeating-linear-gradient(#0000 0 calc(var(--gradient) - 3px), #000 0 var(--gradient));
+ content: "";
+ width: calc(var(--size)/2 + var(--margin));
+ float: left;
+ height: 100%;
+ shape-outside: repeating-linear-gradient(#0000 0 calc(var(--gradient) - 3px), #000 0 var(--gradient));
 }
+
 
 .hexagon {
-  background-color: black;
+ background-color: black;
 }
 
+
 </style>
+
 
 </head>
 <body>
 
+
 <div class="main">
-  <div class="container">
-    <div class="hexagon"></div>
-    <div class="hexagon"></div>
-    <div class="hexagon"></div>
-    <div class="hexagon"></div>
-    <div class="hexagon"></div>
-    <div class="hexagon"></div>
-    <div class="hexagon"></div>
-    <div class="hexagon"></div>
-  </div>
+ <div class="container">
+   <div class="hexagon"></div>
+   <div class="hexagon"></div>
+   <div class="hexagon"></div>
+   <div class="hexagon"></div>
+   <div class="hexagon"></div>
+   <div class="hexagon"></div>
+   <div class="hexagon"></div>
+   <div class="hexagon"></div>
+ </div>
 </div>
 
 </body>
 </html>
-
 
 */
