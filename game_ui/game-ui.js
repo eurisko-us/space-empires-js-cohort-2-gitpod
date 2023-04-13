f39451948fdcbd8d971345a9dc257d04cf3810c2
Charlie Weinberger <364charliebrown@gmail.com>
2023-04-13 16:16:22 +0000

    started to add  hexagon ui code

--- game_ui/game-ui.js
+++ game_ui/game-ui.js
@@ -211,3 +211,67 @@ function updateSquareInfo(i, j) {
 
     }
 }
+
+/*
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<style>
+
+.main {
+  display: flex;
+  --size: 100px;
+  --margin: 4px;
+  --gradient: calc(var(--size) * 1.732 + 4 * var(--margin) - 1px); 
+}
+
+.container {
+  font-size: 0;
+}
+
+.container div {
+  width: var(--size);
+  margin: var(--margin);
+  height: calc(var(--size) * 1.1547);
+  display: inline-block;
+  clip-path: polygon(0% 25%, 0% 75%, 50% 100%, 100% 75%, 100% 25%, 50% 0%);
+  margin-bottom: calc(var(--margin) - var(--size) * 0.2886);
+}
+
+.container::before {
+  content: "";
+  width: calc(var(--size)/2 + var(--margin));
+  float: left;
+  height: 100%;
+  shape-outside: repeating-linear-gradient(#0000 0 calc(var(--gradient) - 3px), #000 0 var(--gradient));
+}
+
+.hexagon {
+  background-color: black;
+}
+
+</style>
+
+</head>
+<body>
+
+<div class="main">
+  <div class="container">
+    <div class="hexagon"></div>
+    <div class="hexagon"></div>
+    <div class="hexagon"></div>
+    <div class="hexagon"></div>
+    <div class="hexagon"></div>
+    <div class="hexagon"></div>
+    <div class="hexagon"></div>
+    <div class="hexagon"></div>
+  </div>
+</div>
+
+</body>
+</html>
+
+
+*/
